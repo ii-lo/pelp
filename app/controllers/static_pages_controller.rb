7e4de806cf5f193377883cb2efe8fce53fb64a20
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2014-10-12 15:31:47 +0200

    Added styles & stuff

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     flash[:error] = "Przykladowy error"
+    flash[:notice] = "Przykladowy notice"
+    flash[:info] = "Przykladowe info"
+    flash[:alert] = "Przykladowy alert"
   end
 end
