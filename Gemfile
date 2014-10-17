375ba22be8cf6d51d90a3e57a176db132760c9fd
Marek Kaput <jjkbtv@gmail.com>
2014-10-17 17:50:55 +0200

    Added string_to_pastel helper.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'thin'
 # forms
 gem 'simple_form'
 
+gem 'colormath'
+
 group :development, :test do
   gem 'rspec-rails', '~> 3.1.0'
   gem "factory_girl_rails", "~> 4.4.1"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
+require 'colormath'
+
 module ApplicationHelper
+    def string_to_pastel(str)
+        mkcolor(hash(str) % 0xFF, 240, 160).compact_hex
+    end
+
+    private def mkcolor(h, s, l)
+        ColorMath::HSL.new(h.to_f / 0xFF, s.to_f / 0xFF, l.to_f / 0xFF)
+        # ColorMath::HSL.new(h, s, l)
+    end
+
+    private def hash(str)
+        h = 0
+        str.each_byte do |b|
+            h += (b << 6) + (b << 16) - b;
+        end
+        h
+    end
 end
