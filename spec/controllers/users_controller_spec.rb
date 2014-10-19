922943fb6d6186990282342f66f6ecbd78aaf15b
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2014-10-19 16:14:22 +0200

    Added simplecov

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  #describe "GET new" do
-    #it "returns http success" do
-      #get :new
-      #expect(response).to have_http_status(:success)
-    #end
-  #end
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
   describe "POST create" do
     context "valid user" do
