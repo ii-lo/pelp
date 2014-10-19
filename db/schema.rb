0c6ffc892e67a42feae4be04b4c0f9855f3eb849
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2014-10-19 17:46:54 +0200

    Added ThumbUploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019115452) do
+ActiveRecord::Schema.define(version: 20141019154143) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20141019115452) do
     t.string   "description"
     t.boolean  "private",     default: false
     t.string   "header"
+    t.string   "thumb"
   end
 
   create_table "exams", force: true do |t|
--- spec/uploaders/thumb_uploader_spec.rb
+++ spec/uploaders/thumb_uploader_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 require 'carrierwave/test/matchers'
 
-RSpec.describe HeaderUploader do
+RSpec.describe ThumbUploader do
   include CarrierWave::Test::Matchers
 
   before do
     @course = FactoryGirl.create :course
-    HeaderUploader.enable_processing = true
-    @uploader = HeaderUploader.new(@course, :header)
+    ThumbUploader.enable_processing = true
+    @uploader = ThumbUploader.new(@course, :header)
     @uploader.store!(File.open(File.expand_path('../Sydney-harbour-bei-nacht-wallpaper.JPG', __FILE__)))
   end
 
   after do
-    HeaderUploader.enable_processing = false
+    ThumbUploader.enable_processing = false
     @uploader.remove!
   end
 
-  it "scales down a landscape image to be exactly 1170 by 128 pixels" do
-    expect(@uploader).to have_dimensions(1170, 128)
+  it "scales down a landscape image to be exactly 64 by 64 pixels" do
+    expect(@uploader).to have_dimensions(64, 64)
   end
 end
