12c876c6f215c40841b5ead5e51cc557d9cc2c90
Marek Kaput <jjkbtv@gmail.com>
2014-11-02 14:05:11 +0100

    Meh

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -11,26 +11,25 @@ function test() {
     $.each(['sent', 'received'], function(i, v) {
         preparePagination(v);
     });
+}
 
 $(document).ready(test);
 $(document).on('page:load', test);
 
-$(document).ready(function() {
-    $('#refresh-btn').on('click', function(e) {
-        e.preventDefault();
-        var btn = $(this).button('loading');
+$('#refresh-btn').on('click', function(e) {
+    e.preventDefault();
+    var btn = $(this).button('loading');
 
-        setTimeout(function() {
-            btn.button('reset');
-        }, 5000);
-    });
+    setTimeout(function() {
+        btn.button('reset');
+    }, 5000);
+});
 
-    $('#send-msg-btn').on('click', function() {
-        var btn = $(this).button('loading');
+$('#send-msg-btn').on('click', function() {
+    var btn = $(this).button('loading');
 
-        setTimeout(function() {
-            btn.button('reset');
-            $('#compose-modal').modal('hide');
-        }, 5000);
-    });
+    setTimeout(function() {
+        btn.button('reset');
+        $('#compose-modal').modal('hide');
+    }, 5000);
 });
