8ef2d04307c1e8a13e6e1b7443d7b4e1262109f1
Marek Kaput <jjkbtv@gmail.com>
2014-11-02 13:41:17 +0100

    Designed message composing form and few minor improvements.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,23 +1,36 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function preparePagination(str) {
+    $('.' + str).on('click', function(e) {
+        e.preventDefault();
+        var re = /=([0-9]+)/;
+        var found = e.target.href.match(re);
+        $.post('messages/paginate', { page: found[1], type: str });
+    });
+}
 
-var prepare_pagination = function(str)
-{
-  $('.' + str).on('click', function(e)
-  {
-    e.preventDefault();
-    var re = /=([0-9]+)/;
-    var found = e.target.href.match(re);
-    $.post("messages/paginate",
-      { page: found[1], type: str }
-    )
-  })
+function test() {
+    $.each(['sent', 'received'], function(i, v) {
+        preparePagination(v);
+    });
 }
-var test = function()
-{
-  $.each(['sent', 'received'], function(ind, a) {
-    prepare_pagination(a);
-  });
-}
+
 $(document).ready(test);
 $(document).on('page:load', test);
+
+$(document).ready(function() {
+    $('#refresh-btn').on('click', function() {
+        var btn = $(this).button('loading');
+
+        setTimeout(function() {
+            btn.button('reset');
+        }, 5000);
+    });
+
+    $('#send-msg-btn').on('click', function() {
+        var btn = $(this).button('loading');
+
+        setTimeout(function() {
+            btn.button('reset');
+            $('#compose-modal').modal('hide');
+        }, 5000);
+    });
+});
