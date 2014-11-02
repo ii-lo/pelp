8315212852cac6391c5c61b8dc3f9ec062c523b2
Marek Kaput <jjkbtv@gmail.com>
2014-11-02 18:13:52 +0100

    Provide unread messages as js object.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def index
     @user = current_user
-    @unread = @user.received_messages.paginate(per_page: 30, page: params[:received_page]) # gonna improve that
-    @sent = @user.sent_messages.paginate(per_page: 30, page: params[:sent_page])
+    @unread = @user.received_messages
+    @unread_json = ActiveModel::ArraySerializer.new(@unread, each_serializer: MessageSerializer).to_json
   end
 
   def page
