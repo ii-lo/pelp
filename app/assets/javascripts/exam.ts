92018258b84e665c8d636ff533f8d5324a47b728
Marek Kaput <jjkbtv@gmail.com>
2014-11-30 13:07:00 +0100

    Added tooltip to exam navbar items.

--- app/assets/javascripts/exam.ts
+++ app/assets/javascripts/exam.ts
@@ -3,6 +3,8 @@
 //= require cclock
 
 $(function () {
+    $('[data-toggle="tooltip"]').tooltip({delay: {show: 600}});
+
     $('[href="/exam/exit"]').on('click', function (e:JQueryEventObject) {
         if (!confirm("Czy na pewno chcesz zakończyć egzamin?")) {
             e.preventDefault();
