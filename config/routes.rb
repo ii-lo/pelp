f11b30593752cd8c041b8234ada2f6105699c115
Marek Kaput <jjkbtv@gmail.com>
2014-11-30 11:36:11 +0100

    Little changes in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users, path: ''
   resources :users
+
   authenticated :user do
     root 'users#dashboard', as: :dashboard
   end
+
   authenticate :user do
     resources :courses do
       member do
@@ -15,20 +17,23 @@ Rails.application.routes.draw do
         get '/exams/:exam_id' => :exam, as: :exam
       end
     end
-    resources :messages
-    get '/user_exams/new/:id' => 'user_exams#new', as: :new_user_exam
-    get '/user_exams/question' => 'user_exams#question', as: :question_user_exam
-    post '/user_exams/answer' => 'user_exams#answer', as: :answer_user_exam
 
-    get 'calendar' => 'calendar#show', as: :calendar
+    resources :messages
     #get 'messages' => 'messages#index', as: :messages
     get 'messages/ajax/page' => 'messages#page'
     get 'messages/ajax/test' => 'messages#test'
+
+    get '/exam/new/:id' => 'user_exams#new', as: :new_user_exam
+    get '/exam/question' => 'user_exams#question', as: :question_user_exam
+    post '/exam/answer' => 'user_exams#answer', as: :answer_user_exam
+
+    get 'calendar' => 'calendar#show', as: :calendar
   end
 
   unauthenticated do
     root 'static_pages#home'
   end
+
   get '/help' => 'static_pages#help', as: :help
   get '/privacy' => 'static_pages#privacy', as: :privacy
   get '/rules' => 'static_pages#rules', as: :rules
