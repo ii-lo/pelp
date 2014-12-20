49ed5ed34232424562930354592c50cf63c566ed
Marek Kaput <jjkbtv@gmail.com>
2014-12-20 15:58:49 +0100

    New navbar

---
+++ app/assets/javascripts/application.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../../tsd/tsd.d.ts" />
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+
+$(function () {
+    var SHRINK_ON = 10;
+
+    $(window).on('load scroll touchmove', function () {
+        $('.navbar').toggleClass('tiny', $(document).scrollTop() > SHRINK_ON);
+    });
+});
