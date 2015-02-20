9990f7148bff724edc302b69cb235b9d402a9bf2
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2015-02-20 18:45:33 +0100

    Routes for lesson categories

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       end
     end
     resources :courses do
+      resources :lesson_categories, except: [:new, :edit]
       member do
         get :add_user
         post :check_password
